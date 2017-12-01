1613a3e787269426518cf1c874666e784e08260e
Toby Smith <toby@tismith.id.au>
2017-12-01 11:22:58 +1000

    Make the response JSON

--- Main.hs
+++ Main.hs
@@ -1,9 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
-import Web.Scotty
+import Web.Scotty (scotty, get, json, param)
+import GHC.Generics
+import Data.Aeson (FromJSON, ToJSON)
+
+
+data Beam = Beam {
+    beamer :: String
+    } deriving (Show, Generic)
+
+instance ToJSON Beam
+instance FromJSON Beam
+
+beamResponse :: String -> Beam
+beamResponse beam = Beam {beamer = beam}
 
 main :: IO ()
 main = scotty 3000 $ do
+  get "/" $ do
+    json $ beamResponse "beam"
   get "/:word" $ do
     beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+    json $ beamResponse beam
