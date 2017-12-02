7706e462c6264a801ea475e0bafdcf065c7000e1
Toby Smith <toby@tismith.id.au>
2017-12-02 11:54:36 +1000

    TIdy up bugs.sql

--- bugs.sql
+++ bugs.sql
@@ -1,5 +1,5 @@
 create table bugs (jira_id text unique not null, url text, jira_status text, assignment text, test_status text, comments text);
-insert into bugs values ("IM-1571", "https://cvs.opengear.com:8081/browse/IM-1571", "Resolved", "davidb", "Pass", "all good");
-insert into bugs values ("IM-42", "https://cvs.opengear.com:8081/browse/IM-42", "Resolved", null, null, null);
-insert into bugs values ("IM-23", "https://cvs.opengear.com:8081/browse/IM-23", "Open", null, "Fail", "all sorts of broken");
-insert into bugs values ("IM-1462", "https://cvs.opengear.com:8081/browse/IM-1462", "Open", null, "Pass", null);
+insert into bugs values ("IM-1571", "https://source/browse/IM-1571", "Resolved", "toby", "Pass", "all good");
+insert into bugs values ("IM-42", "https://source/browse/IM-42", "Resolved", null, null, null);
+insert into bugs values ("IM-23", "https://source/browse/IM-23", "Open", null, "Fail", "all sorts of broken");
+insert into bugs values ("IM-1462", "https://source/browse/IM-1462", "Open", null, "Pass", null);
