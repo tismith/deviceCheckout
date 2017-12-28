313fb3fb73eb22605aa408e6507e64b6a9ab96a4
Toby Smith <toby@tismith.id.au>
2017-12-28 10:18:53 +1000

    General tidy up

--- src/Templates.hs
+++ src/Templates.hs
@@ -9,6 +9,9 @@ import Data.Maybe (maybe, isNothing)
 
 import Types(Bug(..), TestStatus(..))
 
+bootstrap4 :: H.AttributeValue
+bootstrap4 = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+
 showMaybe :: (Show s) => Maybe s -> String
 showMaybe = maybe "" show
 
@@ -25,10 +28,10 @@ options possible current = mapM_
         H.!? (maybeEq x current, A.selected "selected")
         $ H.toMarkup $ show x) possible
 
-optionsMaybe :: (Show a, Eq a) => [a] -> Maybe a -> H.Markup
+optionsMaybe :: (Show a, Eq a, Foldable t ) => t a -> Maybe a -> H.Markup
 optionsMaybe possible current = do
     H.option H.! A.value "-"
-        H.!? (isNothing $ current, A.selected "selected") $ "-"
+        H.!? (isNothing current, A.selected "selected") $ "-"
     options possible current
 
 bugRow :: Bug -> H.Markup
@@ -37,21 +40,30 @@ bugRow bug = do
         H.! A.name (H.toValue $ jiraId bug)
         H.! A.action "/bugs"
         H.! A.method "post" $ do
-        H.input H.! A.type_ "hidden" H.! A.name "jiraId" H.! A.value (H.toValue $ jiraId bug) H.! A.class_ "form-control"
+        H.input H.! A.type_ "hidden" H.! A.name "jiraId"
+            H.! A.value (H.toValue $ jiraId bug) H.! A.class_ "form-control"
         H.td $ H.toMarkup $ jiraId bug
-        H.td $ H.a H.! A.href (H.toValue $ maybe "" id $ url bug) $ H.toHtml $ maybe "" id $ url bug
+        H.td $ H.a H.! A.href (H.toValue $ maybe "" id $ url bug)
+            $ H.toHtml $ maybe "" id $ url bug
         H.td $ H.toMarkup $ showMaybe $ jiraStatus bug
-        H.td $ H.input H.! A.type_ "text" H.! A.name "assignment" H.! A.value (H.toValue $ maybe "" id $ assignment bug) H.! A.class_ "form-control"
+        H.td $ H.input H.! A.type_ "text" H.! A.name "assignment"
+            H.! A.class_ "form-control"
+            H.! A.value (H.toValue $ maybe "" id $ assignment bug)
         H.td $ H.select H.! A.name "testStatus" H.! A.class_ "form-control" $ do
             optionsMaybe (allValues :: [TestStatus]) (testStatus bug)
-        H.td $ H.input H.! A.type_ "text" H.! A.name "comments" H.! A.value (H.toValue $ maybe "" id $ comments bug) H.! A.class_ "form-control"
-        H.td $ H.input H.! A.type_ "submit" H.! A.value "submit" H.! A.class_ "btn btn-primary"
+        H.td $ H.input H.! A.type_ "text" H.! A.name "comments"
+            H.! A.class_ "form-control"
+            H.! A.value (H.toValue $ maybe "" id $ comments bug)
+        H.td $ H.input H.! A.type_ "submit"
+            H.! A.class_ "btn btn-primary"
+            H.! A.value "submit"
 
 bugList :: (Foldable t) => t Bug -> TL.Text
 bugList bugs = renderHtml $ do
     H.head $ do
         H.title "Bug list"
-        H.link H.! A.rel "stylesheet" H.! A.href "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+        H.link H.! A.rel "stylesheet"
+            H.! A.href bootstrap4
 
     H.body $ do
         H.div H.! A.class_ "container" $ do
