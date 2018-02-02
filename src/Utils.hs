32602c7330028ed33c40f3f07903c684121e222f
Toby Smith <toby@tismith.id.au>
2018-02-02 15:49:36 +1000

    Add some basic doctests

--- src/Utils.hs
+++ src/Utils.hs
@@ -2,12 +2,26 @@ module Utils where
 
 import Data.Maybe (maybe)
 
+-- | Show a maybe, overriding the nothing case
+-- >>> showMaybe Nothing
+-- ""
+-- >>> showMaybe (Just "test")
+-- "\"test\""
 showMaybe :: (Show s) => Maybe s -> String
 showMaybe = maybe "" show
 
+-- | Generate all the values for a particular type
+-- >>> data TestType = A | B | C deriving (Bounded, Enum, Show)
+-- >>> allValues :: [TestType]
+-- [A,B,C]
 allValues :: (Bounded a, Enum a) => [a]
 allValues = [minBound ..]
 
+-- | Eq over Maybe, with Nothing mapping to false
+-- >>> maybeEq 1 (Just 1)
+-- True
+-- >>> maybeEq 1 Nothing
+-- False
 maybeEq :: (Eq a) => a -> Maybe a -> Bool
 maybeEq value (Just value') = value == value'
 maybeEq _ _  = False
